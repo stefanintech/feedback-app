a2392dab2abd87910b5ca9a4df9743e673005826
Stefan <stefantaitano@gmail.com>
2023-07-04 23:17:14 -0600

    add proptype for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,21 @@
-function Header() {
+import PropTypes from 'prop-types'
+
+function Header({ text }) {
   return (
     <header>
         <div className="container">
-            <h2>Schools Feedback UI</h2>
+            <h2>{text}</h2>
         </div>
     </header>
   )
+}
+
+Header.defaultProps = {
+    text: 'Schools Feedback UI',
+}
+
+Header.propTypes = {
+    text: PropTypes.string,
 }
 
 export default Header
