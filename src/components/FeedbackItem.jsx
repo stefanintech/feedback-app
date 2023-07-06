2c5ba0abed20842722cb85576512da30def85b0a
Stefan <stefantaitano@gmail.com>
2023-07-05 22:08:03 -0600

    create data folder

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -4,17 +4,10 @@ function FeedbackItem() {
   const [rating, setRating] = useState(1)
   const [text, setText] = useState('Example of feedback item.')
 
-  const handleClick = () => {
-    setRating((prev) => {
-        return prev + 1
-    })
-  }
-
   return (
     <div className="card">
       <div className="num-display">{rating}</div>
       <div className="text-display">{text}</div>
-      <button onClick={handleClick}>Click me!</button>
     </div>
   )
 }
