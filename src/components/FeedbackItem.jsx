cdcf0a8db910aed5f17aa4a369a165dc58647a5e
Stefan <stefantaitano@gmail.com>
2023-07-06 20:14:04 -0600

    add delete function for feedback list

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,10 +1,15 @@
+import {FaTimes} from 'react-icons/fa'
 import PropTypes from 'prop-types'
 import Card from "./shared/Card"
 
-function FeedbackItem({item}) {
+function FeedbackItem({ item, handleDelete }) {
+
   return (
     <Card >
       <div className="num-display">{item.rating}</div>
+      <button onClick={() => handleDelete(item.id)} className='close'>
+        <FaTimes color='white' />
+      </button>
       <div className="text-display">{item.text}</div>
     </Card>
   )
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types'
 import FeedbackItem from './FeedbackItem'
 
-function FeedbackList({feedback}) {
+function FeedbackList({feedback, handleDelete}) {
     console.log(feedback)
     if (!feedback || feedback.length === 0) {
         return <p>No Feedback Available</p>
@@ -10,8 +10,7 @@ function FeedbackList({feedback}) {
     return (
         <div className="feedback-list">
             {feedback.map((item) => (
-                <FeedbackItem key={item.id}  item={item}/>
-
+                 <FeedbackItem key={item.id}  item={item} handleDelete={handleDelete} />
             ))}
         </div>
   )
