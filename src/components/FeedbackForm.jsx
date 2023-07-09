c0c0b1b7b8a64cc30b749fa20f058943a4a9a319
Stefan <stefantaitano@gmail.com>
2023-07-09 17:21:20 -0600

    add state for text input

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react"
 import Card from "./shared/Card"
 
 function FeedbackForm() {
+  const [text, setText] = useState('')
+  const handleTextChange = (e) => {
+    setText(e.target.value)
+  }
+
   return <Card>
     <form>
-        
+        <h2>How would you rate your service with us?</h2>
+        {/* rating select component */}
+        <div className="input-group">
+          <input onChange={handleTextChange} type="text" placeholder="Write a review" value={text} />
+          <button type="submit">Send</button>
+        </div>
+
     </form>
   </Card>
 }
