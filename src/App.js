381fe3bf18f53f0fa5cdf90c07865ef80c69317e
Stefan <stefantaitano@gmail.com>
2023-07-10 00:33:41 -0600

    add about link

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import FeedbackList from "./components/FeedbackList"
 import FeedbackStats from "./components/FeedbackStats"
 import FeedbackData from "./data/FeedbackData"
 import FeedbackForm from "./components/FeedbackForm"
+import AboutIconLink from './components/AboutIconLink'
 import AboutPage from './pages/AboutPage'
 
 function App() {
@@ -38,6 +39,7 @@ function App() {
                     ></Route>
                     <Route path='/about' element={<AboutPage />} />
                 </Routes>
+                <AboutIconLink />
             </div>
         </Router>
     )
