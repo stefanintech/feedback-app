01596bcb69a6992dbadc611c9626014a745b42d3
Stefan <stefantaitano@gmail.com>
2023-07-10 22:30:01 -0600

    add functions to context

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,3 +1,4 @@
+import {v4 as uuidv4} from 'uuid'
 import { createContext, useState } from "react";
 
 const FeedbackContext = createContext()
@@ -11,9 +12,23 @@ export const FeedbackProvider = ({children}) => {
             rating: 10
         }
     ])
+
+    const addFeedback = (newFeedback) => {
+        newFeedback.id = uuidv4()
+        setFeedback([newFeedback, ...feedback])
+}
+
+    const deleteFeedback = (id) => {
+        if(window.confirm('Are you sure you want to delete?')) {
+            setFeedback(feedback.filter((item) => item.id !== id))
+
+        }
+    }
     
     return <FeedbackContext.Provider value={{
         feedback,
+        deleteFeedback,
+        addFeedback,
     }}>
         {children}
     </FeedbackContext.Provider>
