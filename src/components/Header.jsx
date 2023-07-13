5480e7d8512c61da2d016690fa2c84ee28b9ff46
Stefan <stefantaitano@gmail.com>
2023-07-13 16:23:32 -0600

    removed unsused import

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,5 @@
-import { color } from 'framer-motion'
 import PropTypes from 'prop-types'
-import {Link} from 'react-router-dom'
+
 
 function Header({ text, bgColor, textColor }) {
     const headerStyles = {
